311d6c4b9f90086c27b8010b1a0d1a554a3156ee
22030366 <22030366@10.172.135.129>
2023-05-25 11:50:42 +0800

    first push to P00 repository in GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ * 22030366, 25 May 2023 9:49:54 am
+ */
+
+public class Calculator {
+
+	public int add (int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract (int a, int b) {
+		return a - b;
+	}
+	
+	public int multiple (int a, int b) {
+		return a * b;
+	}
+	
+	public int divide (int a, int b) {
+		if (b == 0) {
+			throw new IllegalArgumentException();
+		}
+		return a / b;
+	}
+}
